4a79492619d9bdea79d9370597b1090a21df7261
buivanson994 <32159673+buivanson994@users.noreply.github.com>
2017-10-25 15:56:00 +0700

    up
    up

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<div style="text-align: center; color: red">
+		<h1>xin chào anh đức đẹp trai</h1>
+	</div>
+	<div style="text-align: center;  color: blue">
+		<h1>xin chào anh đức đẹp trai</h1>
+	</div>
+	<div style="text-align: center; color: yellow">
+		<h1>xin chào anh đức đẹp trai</h1>
+	</div>
+	<div style="text-align: center; color: aqua">
+		<h1>xin chào anh đức đẹp trai</h1>
+	</div>
+</body>
+</html>
